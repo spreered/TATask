88c82824f9b21e4aa53b075ac2f6312ea95be0bc
spreered <spreered@gmail.com>
2018-11-13 14:32:31 +0800

    step 7: add task show page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,6 +2,11 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
-
+  def show
+    @task = Task.find_by(id: params[:id])
+    if @task.nil?
+      return redirect_to root_path
+    end
+  end
 
 end
