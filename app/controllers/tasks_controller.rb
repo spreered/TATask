83054cdd5c580238f5b4bbeb420484b53d843570
spreered <spreered@gmail.com>
2018-11-13 14:54:04 +0800

    step 7: add task new page and create task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,11 +2,28 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
   def show
     @task = Task.find_by(id: params[:id])
-    if @task.nil?
-      return redirect_to root_path
+    return redirect_to root_path if @task.nil?
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to task_path(@task)
+    else
+      render new_task_path
     end
   end
 
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
 end
