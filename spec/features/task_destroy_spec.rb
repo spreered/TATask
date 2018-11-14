e8f203359171cde5a88554b6c9587cee18d57f3c
spreered <spreered@gmail.com>
2018-11-14 17:12:02 +0800

    step 8: add delete task feature test

---
+++ spec/features/task_destroy_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.feature "TasksDestroy", type: :feature do
+  before(:each) do
+    @task = FactoryBot.create(:task)
+  end
+  scenario "delete tasks from index pages" do
+    visit tasks_path
+    within("form[action^='/tasks/#{@task.id}']"){
+      expect{
+          click_button 'x'
+      }.to change{ Task.count }.by(-1)
+    }
+  end
+    scenario "delete tasks from task show pages" do
+    visit task_path(@task)
+    expect{
+        click_link 'remove task'
+    }.to change{ Task.count }.by(-1)
+
+  end
+end
