4b883d280eae52901d219ce947643e815cc82915
spreered <spreered@gmail.com>
2018-11-14 15:42:59 +0800

    step 8: add testing show task

---
+++ spec/features/task_show_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.feature "TasksShow", type: :feature do
+  before(:each) do
+    @task = FactoryBot.create(:task)
+  end
+  scenario "index page can visit show page" do
+    visit tasks_path
+    click_link 'task title 1'
+    expect(page).to have_content 'task title'
+    expect(page).to have_content 'task content' 
+  end
+end
