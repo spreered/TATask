982bd77cb5af39cf520aa16c9dd5e6018d753ceb
spreered <spreered@gmail.com>
2018-11-15 17:47:53 +0800

    step 11: add test case - task order by time

--- spec/features/tasks_index_spec.rb
+++ spec/features/tasks_index_spec.rb
@@ -9,5 +9,17 @@ RSpec.feature "TasksIndex", type: :feature do
     visit tasks_path
     expect(page).to have_content task.title
   end
+  scenario "task would ordered by time" do
+    task_1 = FactoryBot.create(:task, created_at: Time.now.yesterday)
+    task_2 = FactoryBot.create(:task, created_at: Time.now)
+
+    visit tasks_path
+    puts task_1.title
+    puts task_2.title
+    # page.should have_selector("table tbody tr:nth-child(1) td:nth-child(2)", content: task_1.title)
+    # page.should have_selector("table tbody tr:nth-child(2) td:nth-child(2)", content: task_2.title)
+    expect(find('table tbody tr:nth-child(1) td:nth-child(2)')).to have_content(task_2.title)
+    expect(find('table tbody tr:nth-child(2) td:nth-child(2)')).to have_content(task_1.title)
+  end
 
 end
