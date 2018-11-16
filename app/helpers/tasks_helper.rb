0a7006e166b0e653fa83a89a09108fcf0bf1f88a
spreered <spreered@gmail.com>
2018-11-17 01:05:22 +0800

    step 14: add deadline/created_at sort link in tasks#index

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,16 @@
 module TasksHelper
+  def deadline_sort
+    if request.GET['sort'] == 'by_deadline_desc'
+      link_to t('views.tasks.deadline'), tasks_path(sort: 'by_deadline_asc')
+    else
+      link_to t('views.tasks.deadline'), tasks_path(sort: 'by_deadline_desc')
+    end
+  end
+  def created_sort
+    if request.GET['sort'] == 'by_created_desc'
+      link_to t('views.tasks.created_at'), tasks_path(sort: 'by_created_asc')
+    else
+      link_to t('views.tasks.created_at'), tasks_path(sort: 'by_created_desc')
+    end
+  end
 end
