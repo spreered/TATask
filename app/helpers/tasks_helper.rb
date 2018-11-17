45a2536adff8bd9c23195be7d89368f3dd7be810
spreered <spreered@gmail.com>
2018-11-17 11:35:57 +0800

    step 16: sort by priority on tasks#index

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,16 +1,23 @@
 module TasksHelper
   def deadline_sort
-    if request.GET['sort'] == 'by_deadline_desc'
-      link_to t('views.tasks.deadline'), tasks_path(sort: 'by_deadline_asc')
+    if request.GET['sort'] == 'deadline_desc'
+      link_to t('views.tasks.deadline'), tasks_path(sort: 'deadline_asc')
     else
-      link_to t('views.tasks.deadline'), tasks_path(sort: 'by_deadline_desc')
+      link_to t('views.tasks.deadline'), tasks_path(sort: 'deadline_desc')
     end
   end
   def created_sort
-    if request.GET['sort'] == 'by_created_desc'
-      link_to t('views.tasks.created_at'), tasks_path(sort: 'by_created_asc')
+    if request.GET['sort'] == 'created_desc'
+      link_to t('views.tasks.created_at'), tasks_path(sort: 'created_asc')
     else
-      link_to t('views.tasks.created_at'), tasks_path(sort: 'by_created_desc')
+      link_to t('views.tasks.created_at'), tasks_path(sort: 'created_desc')
+    end
+  end
+  def priority_sort
+    if request.GET['sort'] == 'priority_desc'
+      link_to t('views.tasks.priority_t'), tasks_path(sort: 'priority_asc')
+    else
+      link_to t('views.tasks.priority_t'), tasks_path(sort: 'priority_desc')
     end
   end
 end
