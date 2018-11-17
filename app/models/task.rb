8151c446ced458c2a531da989efb6b68040c1a9e
spreered <spreered@gmail.com>
2018-11-17 11:19:37 +0800

    step 16: add priority on tasks, set it to enum

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
   validates :title, presence: true
   scope :ordered, lambda {|*args| order(args.first || 'created_at DESC') }
+  enum priority: %i[low medium high]
 end
