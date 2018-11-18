6d491295b4fabc558476df30b83dc0f6ee5807b3
spreered <spreered@gmail.com>
2018-11-18 23:23:30 +0800

    step 19: add user model and db:seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_18_032248) do
+ActiveRecord::Schema.define(version: 2018_11_18_151133) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 2018_11_18_032248) do
     t.integer "state", default: 0, null: false
     t.index ["state"], name: "index_tasks_on_state"
     t.index ["title"], name: "index_tasks_on_title"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,7 @@
 FactoryBot.define do
+  factory :user do
+    
+  end
   factory :task do
     sequence(:title) { |n| "task title #{n}" }
     sequence(:content) { |n| "task content #{n}" }
