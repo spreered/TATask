191e7093c6777cdf08de5854f0649f8552d7e556
spreered <spreered@gmail.com>
2018-11-19 01:05:14 +0800

    step 19: add user validates and user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) do
+    FactoryBot.create(:user)
+  end
+  it 'is valid with name and email' do
+    user
+    expect(user).to be_valid
+  end
+  it 'is invalid without name' do
+    user_1 = User.new(name:nil, email:'abc@123')
+    expect(user_1.valid?).to be false
+  end
+  it 'is invalid without email' do
+    user_1 = User.new(name:'user 1', email: nil)
+    expect(user_1.valid?).to be false
+  end
+  it 'would check email valid?' do
+    user_1 = User.new(name:'user 1', email: 'abcemail')
+    expect(user_1.valid?).to be false
+  end
 end
