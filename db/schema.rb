d192785f81d8e9e74e17a2f63b015da23e5a3564
spreered <spreered@gmail.com>
2018-11-18 11:25:17 +0800

    step 15: add title , state index to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_17_043318) do
+ActiveRecord::Schema.define(version: 2018_11_18_032248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2018_11_17_043318) do
     t.datetime "deadline"
     t.integer "priority", default: 0, null: false
     t.integer "state", default: 0, null: false
+    t.index ["state"], name: "index_tasks_on_state"
+    t.index ["title"], name: "index_tasks_on_title"
   end
 
 end
