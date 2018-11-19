9e2438b93980879b749200918bbf83316946ca03
spreered <spreered@gmail.com>
2018-11-20 01:09:14 +0800

    step 21: add admin::user#show page, including tasks

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,8 +1,11 @@
 class Admin::UsersController < ApplicationController
+  before_action :set_user, only: %i[edit update show destroy start done]
   def index
     @users = User.page(params[:page]).per(20)
   end
   def show
+    @q = Task.includes(:user).where(users:{id: @user.id}).ransack(params[:q])
+    @tasks = @q.result.page(params[:page]).per(20)
   end
   def new
   end
@@ -14,5 +17,10 @@ class Admin::UsersController < ApplicationController
   end
   def destroy
   end
-  
+  private
+
+  def set_user
+    @user = User.find_by(id: params[:id])
+    return redirect_to admin_root_path if @user.nil?
+  end
 end
