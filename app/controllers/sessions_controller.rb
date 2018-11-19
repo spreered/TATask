17620548324845a638359d1e448cebe14e4116c8
spreered <spreered@gmail.com>
2018-11-19 11:23:22 +0800

    step 20: add session#new (user login)

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      flash[:notice] = t('.notice')
+      login user
+      redirect_to root_path
+    else
+      flash[:alert] = t('.alert')
+      render 'new'
+    end
+  end
+end
---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,10 @@
+module SessionsHelper
+  def login(user)
+    session[:user_id] = user.id
+  end
+  def current_user
+    if session[:user_id]
+      @current_user ||=User.find_by(id: session[:user_id])
+    end
+  end
+end
