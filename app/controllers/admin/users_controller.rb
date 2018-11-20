5fc0f0d60f844ae334c970881da8028454172309
spreered <spreered@gmail.com>
2018-11-20 11:29:21 +0800

    step_21: update/destroy user at admin page

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -8,16 +8,51 @@ class Admin::UsersController < ApplicationController
     @tasks = @q.result.page(params[:page]).per(20)
   end
   def new
+    @user = User.new
   end
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = t('.notice')
+      redirect_to admin_root_path
+    else
+      render :new
+    end
   end
   def edit
+    @user = User.find_by(id: params[:id])
+    unless @user
+      return redirect_to admin_root_path
+    end
   end
   def update
+    @user = User.find_by(id: params[:id])
+    
+    unless @user
+      return redirect_to admin_root_path
+    end
+    update_params = {}
+    user_params.each do |key, value|
+      update_params[key] = value unless value.blank?
+    end
+
+    if @user.update(update_params)
+      flash[:notice] = t('.notice')
+      redirect_to admin_root_path
+    else
+      render :edit
+    end
   end
   def destroy
+    @user.destroy
+    flash[:alert] = t('.notice')
+    redirect_to admin_root_path
   end
   private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 
   def set_user
     @user = User.find_by(id: params[:id])
